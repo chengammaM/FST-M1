ba7ba5d93353005a84b6d7cc09ed87073d3742e0
chengammaM <Chengamma.Mamidigunta@ibm.com>
2025-11-17 18:42:44 +0530

    Update TSHomepageSteps.java

--- Cucumber/Activities/TSHomepageSteps.java
+++ Cucumber/Activities/TSHomepageSteps.java
@@ -1,5 +1,34 @@
-package Cucumber;
+package stepDefinitions;
 
-public class TSHomepageSteps {
 
-}
+	import static org.junit.jupiter.api.Assertions.assertEquals;
+
+	import org.openqa.selenium.By;
+	import org.openqa.selenium.support.ui.ExpectedConditions;
+
+	import io.cucumber.java.en.Given;
+	import io.cucumber.java.en.Then;
+	import io.cucumber.java.en.When;
+
+	public class TSHomepageSteps extends BaseClass {
+		@Given("user is on the TS homepage")
+		public void openTSHomepage() throws Throwable {
+			// Open the browser
+			driver.get("https://training-support.net");
+			assertEquals(driver.getTitle(), "Training Support");
+		}
+
+		@When("the user clicks on the About Us link")
+		public void clickButton() throws Throwable {
+			driver.findElement(By.linkText("About Us")).click();
+		}
+
+		@Then("they are redirected to another page")
+		public void verifyaboutPage() {
+			//Wait for the new page to load
+			wait.until(ExpectedConditions.titleContains("About"));
+			//Assertion
+			assertEquals("About Training Support", driver.getTitle());
+		}
+	}
+
